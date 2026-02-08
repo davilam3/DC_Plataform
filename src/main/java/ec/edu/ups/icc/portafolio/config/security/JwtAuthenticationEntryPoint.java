13cd665b9cbc03352198bafc13c4955765ff734a
Sebastian Cabrera <ccabreram1@est.ups.edu.ec>
2026-02-08 18:11:47 -0500

    object

--- src/main/java/ec/edu/ups/icc/portafolio/config/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/ec/edu/ups/icc/portafolio/config/security/JwtAuthenticationEntryPoint.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
@@ -19,7 +20,9 @@ import java.io.IOException;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
-    private final ObjectMapper objectMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     public JwtAuthenticationEntryPoint(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
